2d5834ded9a547ff4b49654cc846848005406328
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-01-07 11:04:53 +0100

    added cube and sphere and made 3D

--- vector/sketch.js
+++ vector/sketch.js
@@ -1,35 +1,42 @@
 let postion;
 let velocity;
-let size = 48;
+let size = 20;
+boxSize = 600;
 
-function setup() 
-{
-	createCanvas(windowWidth-20, windowHeight-20);
-	postion = createVector(100,100,100);
-	velocity = createVector(random(1,10),random(1,10),random(1,10));
-	
+function setup() {
+  createCanvas(windowWidth - 20, windowHeight - 20, WEBGL);
+  postion = createVector(0, 0, 0);
+  velocity = createVector(random(1, 10), random(1, 10), random(1, 10));
 }
 
-function draw()
-{
-	background(0);
-	postion.add(velocity);
-	size = map(postion.z , 0, width, 5, 64);
-	stroke(255);
-	strokeWeight(2);
-	fill(127);
-	ellipse(postion.x, postion.y, size, size);
+function draw() {
+  orbitControl();
+  lights();
+  background(0);
+  postion.add(velocity);
+  //size = map(postion.z, 0, width, 5, 64);
+  push();
+  noFill();
+  stroke(255);
+  translate(0, 0, -boxSize / 2);
+  box(boxSize, boxSize, boxSize);
 
-	if (postion.x > width || postion.x < 0)
-	{
-		velocity.x = velocity.x * -1;
-	}
-	if (postion.z > width || postion.z < 0)
-	{
-		velocity.z = velocity.z * -1;
-	}
-	if (postion.y > height || postion.y < 0)
-	{
-		velocity.y = velocity.y * -1;
-	}
+  pop();
+
+  //ellipse(postion.x, postion.y, size, size);
+  push();
+  translate(-boxSize / 2 + postion.x, -boxSize / 2 + postion.y, -boxSize + postion.z);
+  fill(255);
+  sphere(size);
+  pop();
+
+  if (postion.x > boxSize || postion.x < 0) {
+    velocity.x = velocity.x * -1;
+  }
+  if (postion.z > boxSize || postion.z < 0) {
+    velocity.z = velocity.z * -1;
+  }
+  if (postion.y > boxSize || postion.y < 0) {
+    velocity.y = velocity.y * -1;
+  }
 }
