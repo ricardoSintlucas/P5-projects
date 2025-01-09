cb74e07611fe35abad06d9701f7597525c90cf98
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-01-09 11:04:25 +0100

    Add a Force project to it

---
+++ UseTheForce/mover.js
@@ -0,0 +1,38 @@
+class Mover {
+  constructor(x, y, mass) {
+    this.position = createVector(x, y);
+    this.velocity = createVector();
+    this.acceleration = createVector(0, 0);
+    this.mass = mass;
+  }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+  show() {
+    noStroke();
+    fill(255, 80);
+    circle(this.position.x, this.position.y, this.mass * 10);
+  }
+
+  checkEdges() {
+    if (this.position.x > width) {
+      this.position.x = width;
+      this.velocity.x *= -1;
+    } else if (this.position.x < 0) {
+      this.velocity.x *= -1;
+      this.position.x = 0;
+    }
+    if (this.position.y > height) {
+      this.velocity.y *= -1;
+      this.position.y = height;
+    }
+  }
+}
---
+++ UseTheForce/sketch.js
@@ -0,0 +1,24 @@
+let movers = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    let mover = new Mover(random(width), random(height), random(1, 20));
+    movers.push(mover);
+  }
+}
+
+function draw() {
+  background(0);
+  for (let mover of movers) {
+    let gravity = createVector(0, 0.2);
+    mover.applyForce(gravity);
+    mover.update();
+    mover.show();
+    mover.checkEdges();
+    if (mouseIsPressed) {
+      let wind = createVector(0.1, 0);
+      mover.applyForce(wind);
+    }
+  }
+}
