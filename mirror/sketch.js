5c02785812c01050d46af3887965bc093c17aadf
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-01-17 09:33:38 +0100

    Update sketch.js

--- mirror/sketch.js
+++ mirror/sketch.js
@@ -1,29 +1,27 @@
 let capture;
 
-function setup() 
-{
-	createCanvas(800, 600);
+function setup() {
+  createCanvas(800, 600);
 
   // Webcam capture (at the size of the window)
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
-	
 }
 
-function draw()
-{
-	background(255);
-
+function draw() {
+  background(255);
+  image(video, 0, 0);
   // Try experimenting with this
   let gridSize = 10;
 
   // The video has pixels just like an image!
-  video.loadPixels();
-  for (let y=0; y<video.height; y+=gridSize) {
-    for (let x=0; x<video.width; x+=gridSize) {
-     
-      
-    }
+  image.loadPixels();
+  for (let y = 0; y < video.height; y += gridSize) {
+    for (let x = 0; x < video.width; x += gridSize) {}
   }
+}
+
+function mouseClicked() {
+  console.log(image.get(mouseX, mouseY));
 }
