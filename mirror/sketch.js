afd15e7b83c87eaeca5fc6d4a747281efaa0c3bb
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-01-22 22:49:02 +0100

    Update sketch.js

--- mirror/sketch.js
+++ mirror/sketch.js
@@ -6,18 +6,20 @@ function setup() {
 
   // Webcam capture (at the size of the window)
   video = createCapture(VIDEO);
+  scale(-1,1); 
   video.size(800, 600);
   video.hide();
 }
 
 function draw() {
   background(0);
-  //image(video, 0, 0);
-  // Try experimenting with this
+  translate(video.width, 0); // Move the origin to the right edge
+  scale(-1, 1); // Flip the canvas horizontally
   let gridSize = 20;
+  
+  // The video has pixels just like an image! 
+ video.loadPixels();
 
-  // The video has pixels just like an image!
-  video.loadPixels();
   for (let y = 0; y < video.height; y += gridSize) {
     for (let x = 0; x < video.width; x += gridSize) {
       let index = (x + y * video.width) * 4;
@@ -26,18 +28,19 @@ function draw() {
       let b = video.pixels[index + 2];
       let grayscale = (r + g + b) / 3;
       //let size = map(grayscale, 0, 255, 2, gridSize);
+      // ellipse(x, y, size, size);
       let rotation = map(grayscale, 0, 255, 0, PI);
       push();
       fill(255);
       noStroke();
-
       translate(x, y);
       rotate(rotation);
       rectMode(CENTER);
       rect(0, 0, 2, gridSize);
-      rect(0, 0, gridSize, 2);
+      rect(0, 0, gridSize, 2); 
       pop();
-      //ellipse(x, y, size, size);
+     
     }
   }
+  
 }
